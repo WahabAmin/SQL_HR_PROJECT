133532d9b0b3f37b2ba97df14bf3f8c5a02eea28
WahabAmin <123979769+WahabAmin@users.noreply.github.com>
2023-08-10 17:45:42 -0800

    Add files via upload

---
+++ Questions.sql
@@ -0,0 +1,28 @@
+-- QUESTIONS
+
+-- 1. What is the gender breakdown of employees in the company?
+
+-- 2. What is the race/ethnicity breakdown of employees in the company?
+
+-- 3. What is the age distribution of employees in the company?
+
+-- 4. How many employees work at headquarters versus remote locations?
+
+-- 5. What is the average length of employment for employees who have been terminated?
+
+-- 6. How does the gender distribution vary across departments and job titles?
+
+-- 7. Which department has the highest turnover rate?
+
+-- 8. What is the distribution of employees across locations by city and state?
+
+-- 9. How has the company's employee count changed over time based on hire and term dates?
+
+-- 10. Has workforce changed over time in terms of diversity?
+
+-- 11.  employee termination count over years?
+
+ -- 12. Top 5 job titles with highest termination rate?
+ 
+ -- 13. Top 10 job titles with highest hirirng rate?
+ 
